12c551bce02c0edaee3d1d15830d41a510ca1aac
jejakawan007 <jejakawan007@gmail.com>
2025-08-15 10:34:10 +0000

    Remove all old files to prepare for fresh structure update

--- frontend/app/test-input/page.tsx
+++
@@ -1,6 +0,0 @@
-import TestInput from '../login/test-input';
-
-export default function TestInputPage() {
-  return <TestInput />;
-}
-
--- frontend/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all available hooks
-export * from './use-toast';
-export * from './useAuth';
-export * from './useDashboard';
-export * from './usePosts';
-export * from './useUsers'; 
